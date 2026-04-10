33d8e16593729b0c85f83e411ec6e7042d4fd5ad
rwus111 <55919737+rwus111@users.noreply.github.com>
2026-04-11 00:11:43 +0700

    test sh

--- bin/github_data.sh
+++ bin/github_data.sh
@@ -1,51 +1,20 @@
-#!/usr/bin/env bash
 # commit to csv_data
+#!/usr/bin/env bash
 
-set -euo pipefail
-
-URL=https://github.com/vietvudanh/vietlott-data.git
-FOLDER=vietlott-data
+FOLDER=vietlott
 DATA_FOLDER=data
-USER="Viet VU"
-EMAIl="vietvudanh@gmail.com"
-VENV=".venv"
 
-# Activate virtual environment if it exists
-if [ -n "$VENV" ]; then
-  source "$VENV/bin/activate"
-fi
-
-git pull --rebase --autostash origin main
-
-# generate data file
-echo "pwd $(pwd)"
 
 export PYTHONPATH="src"
 export LOGURU_LEVEL="INFO"
 
-for product in keno power_655 power_645 power_535 3d 3d_pro bingo18; do
-  python src/vietlott/cli/crawl.py "$product"
-  python src/vietlott/cli/missing.py "$product"
-done
+# Random delay (1s to 5 minutes) to avoid detect as bot
+RANDOM_DELAY=$(( RANDOM % 301 + 1 ))
+sleep $RANDOM_DELAY
 
-python src/render_readme.py
-python src/render_docs.py
-
-#if [[ ! -d "$FOLDER" ]] ; then
-#  git clone $URL $FOLDER
-#fi
-
-#cp -r $DATA_FOLDER $FOLDER/
-
-#cd $FOLDER
-#git pull
-
-# commit and push
-git config user.name "\'$USER\'"
-git config user.email "\'$EMAIl\'"
-git status
-git add $DATA_FOLDER
-git add readme.md
-git add docs/index.html
-git commit -m "update data @ `date +%Y-%m-%d\ %H:%M:%S`"
-git push origin main
+python src/vietlott/cli/crawl.py power_655
+python src/vietlott/cli/missing.py power_655
+python src/vietlott/cli/crawl.py power_645
+python src/vietlott/cli/missing.py power_645
+#python src/vietlott/cli/crawl.py keno
+#python src/vietlott/cli/missing.py keno
